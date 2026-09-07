59dbf8e69d97b8e9c1519476151df7bfc933dbc5
kinohisob2-ux <kinohisob2@gmail.com>
2026-09-07 17:42:08 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8729235570:AAHtlsdBhenTt6CYepGxQtjg7Z5gDKaFoEw")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "2010030869"))
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://kino_akk_hisob_2_uzbekcha_kino_user:trt5tO7ldUi7hWWKuJWepkjZFmZz5Lwz@dpg-d9jj5hmrnols738sdpb0-a.ohio-postgres.render.com/kino_akk_hisob_2_uzbekcha_kino")
+# Aynan shu tokenni ishlating
+BOT_TOKEN = "8729235570:AAHtlsdBhenTt6CYepGxQtjg7Z5gDKaFoEw"  # Yangi token
+ADMIN_ID = 2010030869
+DATABASE_URL = "postgresql://kino_akk_hisob_2_uzbekcha_kino_user:trt5tO7ldUi7hWWKuJWepkjZFmZz5Lwz@dpg-d9jj5hmrnols738sdpb0-a.ohio-postgres.render.com/kino_akk_hisob_2_uzbekcha_kino"
 
 # Telethon uchun
-API_ID = int(os.getenv("API_ID", "38486800"))
-API_HASH = os.getenv("API_HASH", "c5fc7e4d2190b89e5ce8ea01c0369f09")
+API_ID = 38486800
+API_HASH = "c5fc7e4d2190b89e5ce8ea01c0369f09"
